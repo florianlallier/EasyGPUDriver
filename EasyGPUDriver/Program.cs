34e28508bc1cf6e001dfd98eb4d9868781963d13
Florian Lallier <contact@florianlallier.fr>
2021-12-28 19:54:05 +0100

    Create structure

--- EasyGPUDriver/Program.cs
+++ EasyGPUDriver/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace EasyGPUDriver
 {
@@ -10,6 +6,56 @@ namespace EasyGPUDriver
     {
         static void Main(string[] args)
         {
+            Console.Title = "EasyGPUDriver";
+
+            Version currentVersion = GetCurrentVersion();
+            Version lastVersion = GetLastVersion();
+
+            Console.WriteLine($"Current version: {currentVersion}");
+            Console.WriteLine($"Last version: {lastVersion}");
+
+            if (currentVersion.CompareTo(lastVersion) == -1)
+            {
+                Console.Write("\nNew version available! Do you want to download it? [y/N] ");
+
+                ConsoleKeyInfo cki = Console.ReadKey();
+                Console.WriteLine();
+
+                if (cki.Key.ToString() == "Y")
+                {
+                    DownloadDriver();
+                }
+            }
+            else
+            {
+                Console.WriteLine("\nUp-to-date.");
+            }
+
+            Console.Write("\nPress any key to exit... ");
+            Console.ReadKey();
+        }
+
+        private static Version GetCurrentVersion()
+        {
+            string version = "0.0";
+
+            // TODO
+
+            return new Version(version);
+        }
+
+        private static Version GetLastVersion()
+        {
+            string version = "0.0";
+
+            // TODO
+
+            return new Version(version);
+        }
+
+        private static void DownloadDriver()
+        {
+            // TODO
         }
     }
 }
